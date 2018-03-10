42129720677c346d4da10b48bc4a4b01734da367
Ahmad R Yacoub <ah.romel94@gmail.com>
2018-03-10 23:37:43 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ahmed Yacoub on 3/9/18.
+//  Copyright © 2018 Ahmed Yacoub. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
